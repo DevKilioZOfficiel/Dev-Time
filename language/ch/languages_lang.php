f65e01e3e0d2abbef9dde7894ae67b85588490fb
KilioZ <trix-folloow@hotmail.com>
2019-08-27 18:54:09 +0200

    Ajouts de langues
    Portugais (PT & BR), Français Suisse (CH), Français Canadien (CA)

---
+++ language/ch/languages_lang.php
@@ -0,0 +1,178 @@
+<?php
+    //INFO LANGUE
+    $lang['name_language'] = 'Français Suisse';
+    $lang['code_language'] = 'CH';
+    $lang['code_language_small_txt'] = 'ch';
+    $lang['author_language'] = 'KilioZ';
+    $lang['version_language'] = '1.0.0';
+
+    // MENU
+    $lang['navigation'] = "Navigation";
+    $lang['open_menu'] = 'Ouvrir le menu';
+    $lang['close_menu'] = 'Fermer le menu';
+    $lang['news__menu'] = 'Actualité';
+    $lang['search_txt'] = 'Rechercher...';
+
+    // HOME
+    $lang['welcome_message'] = "Bienvenue sur le réseau social des développeurs ! Retrouvez de nombreux développeurs dans divers domaines !";
+
+    $lang['your_account'] = "Votre compte";
+    $lang['settings_account'] = "Mes paramètres";
+    $lang['logout'] = "Déconnexion";
+    $lang['settings_chat'] = "Paramètres des discussions";
+
+    $lang['signin'] = "Connexion";
+    $lang['signup'] = "Inscription";
+    $lang['forgot_password'] = "Mot de passe oublié ?";
+    $lang['about'] = "A Propos de Dev-Time";
+    $lang['terms'] = "Nos conditions d'utilisation";
+    $lang['faq'] = "FAQ";
+    $lang['download'] = "Télécharger";
+
+    $lang['your_email'] = "Votre Email";
+    $lang['your_username'] = "Votre Pseudo";
+    $lang['your_password'] = "Votre Mot de passe";
+    $lang['confirm_your_password'] = "Confirmer votre mot de passe";
+
+    $lang['success_signup'] = "Vous êtes inscrit avec succès !";
+    $lang['account_exist_signup'] = "Un compte existe déjà sous ces identifiants";
+    $lang['pwd_error_txt'] = "Vos mots de passe ne sont pas identique";
+    $lang['error__input_clear'] = "Certains champs sont vide";
+    $lang['login_success'] = "Connexion en cours...";
+    $lang['error__account_not_found'] = "Aucun compte n'existe sur Dev-Time";
+    $lang['error__account_suspend'] = "Votre compte est suspendu";
+    $lang['forgot_password_reset'] = "Rénitialiser";
+
+    $lang['chat_txt'] = "Discussions en cours";
+    $lang['chat_txt2'] = "Disccusions en attente";
+    $lang['settings'] = "Paramètres";
+    $lang['new_conversation'] = "Nouvelle conversation";
+    $lang['add_conversation'] = "Ajouter à la conversation";
+    $lang['blocked'] = "Bloquer";
+
+    $lang['search'] = "Rechercher";
+    $lang['chats'] = "Discussions";
+    $lang['view_all_chat'] = "Voir tous les messages";
+    $lang['notifications'] = "Notifications";
+    $lang['mark_all_read'] = "Marquer tous comme lus";
+    $lang['view_all_notifications'] = "Voir toutes les notifications";
+
+    $lang['online'] = "En Ligne";
+    $lang['offline'] = "Hors Ligne";
+    $lang['coming_soon'] = "Cette option arrive très prochainement sur Dev-Time !";
+
+    // PROFIL
+    $lang['posts_user'] = "Publications";
+    $lang['about_user'] = "A Propos";
+    $lang['followers_user'] = "Abonnements";
+    $lang['projects_user'] = "Projets";
+    $lang['courses_user'] = "Tutoriels";
+
+    $lang['add_project'] = "Ajouter un projet";
+    $lang['project_link'] = "Lien du projet";
+    $lang['button_add_project'] = "Ajouter";
+    $lang['button_close'] = "Fermer";
+    $lang['add_project_success'] = "Votre project à été ajouté avec succès !";
+
+    $lang['publish_profile_to'] = "à publié sur le profil de %s";
+    $lang['share_profile_to'] = "à partagé la publication de %s";
+    $lang['bio'] = "Biographie";
+    $lang['infos_profile'] = "Informations du profil";
+    $lang['repport_profile'] = "Signaler le profil";
+    $lang['repport_profile_success'] = "Ce profil est signalé avec succès !";
+    $lang['repport_profile_error'] = "Impossible de signaler ce profil";
+    $lang['blocked_profile'] = "Bloquer le profil";
+    $lang['deblocked_profile'] = "Débloquer le profil";
+    $lang['blocked_profile_success'] = "Vous avez bloqué ce profil";
+    $lang['deblocked_profile_success'] = "Vous avez débloqué ce profil";
+
+    $lang['edit__username_success'] = "Vous avez modifié votre pseudo avec succès !";
+    $lang['edit__username_error'] = "Oups ! Ce pseudo existe déjà";
+    $lang['edit__url_success'] = "Vous avez modifié votre lien avec succès !";
+    $lang['edit__url_error'] = "Oups ! Ce lien existe déjà";
+    $lang['edit__logo_success'] = "Vous avez modifié votre logo avec succès !";
+    $lang['edit__banner_success'] = "Vous avez modifié votre bannière avec succès !";
+    $lang['edit__password_success'] = "Vous avez modifié votre mot de passe avec succès !";
+    $lang['edit__bio_success'] = "Vous avez modifié votre biographie avec succès !";
+    $lang['edit__your_username'] = "Votre pseudo";
+    $lang['edit__your_url'] = "Votre lien";
+    $lang['edit__choose_file'] = "Choisir un fichier";
+    $lang['edit__password'] = "Mot de passe";
+    $lang['edit__bio'] = "Votre Biographie";
+    $lang['edit__button_settings'] = "Modifier mes paramètres";
+    $lang['your_network'] = "Vos réseaux sociaux";
+    $lang['your_twitter'] = "Votre Twitter (Ex: @DevTimeEU)";
+    $lang['your_discord'] = "Votre Discord (Ex: 368832865850621952)";
+    $lang['your_twitch'] = "Votre Twitch (Ex: yt_trix_folloow)";
+    $lang['your_yt'] = "Votre Chaîne YouTube (Ex: UCqK3KdmruqJqKZ8uB-_x_5Q)";
+    $lang['update_network'] = "Mettre à jour mes réseaux";
+    $lang['twitter_success'] = "Votre Twitter est mis à jour";
+    $lang['twitch_success'] = "Votre Twitch est mis à jour";
+    $lang['discord_success'] = "Votre Discord est mis à jour";
+    $lang['yt_success'] = "Votre YouTube est mis à jour";
+
+    $lang['poll_success'] = "Votre vote à été pris en compte";
+    $lang['poll_voted'] = "Vous avez déjà voté !";
+    $lang['poll_error'] = "Oups ! Il est impossible de voter";
+    $lang['poll_login_required'] = "Vous devez être connecté pour voter !";
+
+    // POSTS
+    $lang['message'] = "Message";
+    $lang['message_button'] = "Publier mon message";
+    $lang['link'] = "Lien";
+    $lang['link_button'] = "Publier mon lien";
+    $lang['poll'] = "Sondage";
+    $lang['add_button_poll'] = "Ajouter une réponse";
+    $lang['poll_input'] = "Réponse";
+    $lang['remove_button_poll'] = "Supprimer la réponse";
+    $lang['poll_button'] = "Publier mon sondage";
+    $lang['poll_title'] = "Titre du sondage";
+
+    $lang['message_success'] = "Votre message à été publié !";
+    $lang['message_error'] = "Oups ! Ce message à déjà été envoyé !";
+    $lang['link_success'] = "Votre lien à été publié !";
+    $lang['link_error'] = "Oups ! Ce lien à déjà été envoyé !";
+    $lang['poll_success'] = "Votre sondage à été publié !";
+    $lang['poll_error'] = "Oups ! Ce sondage à déjà été envoyé !";
+    $lang['post_delete'] = "Cette publication vient d'être supprimé !";
+    $lang['post_delete_error'] = "Cette publication est déjà supprimé !";
+
+    // 1.0.1 AJOUTS
+    $lang['developer_account'] = "Espace développeur";
+    $lang['developer_bio'] = "Vôtre biographie de développeur";
+    $lang['developer_link'] = "Lien";
+    $lang['developer_name'] = "Nom de vôtre lien";
+    $lang['developer_add'] = "Ajouter un lien";
+    $lang['developer_remove'] = "Supprimer le lien";
+    $lang['developer_update'] = "Mettre à jour";
+    $lang['developer_update_success'] = "Vous avez mis à jour vos informations !";
+    $lang['developer_lists'] = "Listes des demandes";
+    $lang['developer_list_valid'] = "Valider la demande";
+    $lang['developer_list_refuse'] = "Refuser la demande";
+    $lang['developer_bio_updated'] = "Vôtre biographie à été mise à jour !";
+    $lang['developer_links_updated'] = "Vos liens sont mis à jour !";
+
+    $lang['developer_discord_message'] = "Chaque développeur doit être sur notre Discord ! <a href='https://discord.gg/MwZWYMY'>Rejoindre maintenant !</a>";
+    $lang['developer_demande_language'] = "Langage (Multiple sélection)";
+    $lang['developer_demande_presentation'] = "Présentez-vous";
+    $lang['developer_demande_finish_button'] = "Faire ma demande";
+    $lang['developer_error'] = "Erreur: Certains champs sont vides !";
+    $lang['developer_demande_error'] = "Vous avez déjà faite une demande !";
+    $lang['developer_demande_success'] = "Votre demande vient d'être effectué ! Vous allez recevoir une réponse dans les 7 prochains jours !";
+    $lang['developer_demandes'] = "Vos demandes";
+    $lang['developer_demandes_wait'] = "En attente";
+    $lang['developer_demandes_deny'] = "Refusée";
+    $lang['developer_demandes_success'] = "Validée";
+    $lang['developer_languages'] = "Langages";
+    $lang['developer_demande_user'] = "Demande de";
+    $lang['developer_button_valide'] = "Valider";
+    $lang['developer_button_deny'] = "Refuser";
+    $lang['developer_demande_error_admin'] = "Vous avez refusé cette demande !";
+    $lang['developer_demande_success_admin'] = "Votre avez validé cette demande !";
+
+    $lang['developer_faq_demande_title'] = "A quoi sert ma demande ?";
+    $lang['developer_faq_demande_description'] = "Votre demande pour devenir développeur va permettre de vous référencer sur l'application. De plus, une fois votre demande \"Validée\", vous pourrez modifier votre page.<br>
+    Une demande peut pendre 1 semaine à se faire. Cependant, vous ne serrez pas référencé directement sur l'application, il faudra que nous mettions à jour nos applications sur les divers OS"; //<br> = sauter une Ligne
+    $lang['developer_faq_languages_title'] = "Un langage de programmation n'est pas dans la liste. Comment faire ?";
+    $lang['developer_faq_language_description'] = "Nous mettons principalement les langages les plus utilisés, c'est à dire que les langages peu utilisés ne sont pas forcement mis. Cependant, s'il s'agit d'un langage utilisé par de nombreux développeurs, nous mettrons à jour afin d'agrandir notre liste.";
+?>
---
+++ language/pt/languages_lang.php
@@ -0,0 +1,178 @@
+<?php
+    // INFO LINGUA
+    $lang ['name_language'] = 'Português';
+    $lang ['code_language'] = 'PT';
+    $lang ['code_language_small_txt'] = 'pt';
+    $lang ['author_language'] = 'Reydux';
+    $lang ['version_language'] = '1.0.0';
+
+    // MENU
+    $lang ['navigation'] = "Navegação";
+    $lang ['open_menu'] = 'Abrir menu';
+    $lang ['close_menu'] = 'Fechar menu';
+    $lang ['news__menu'] = 'Notícias';
+    $lang ['search_txt'] = 'Pesquisar ...';
+
+    // HOME
+    $lang ['welcome_message'] = "Bem-vindo à rede social dos desenvolvedores! Encontre muitos desenvolvedores em vários campos!";
+
+    $lang ['your_account'] = "Sua conta";
+    $lang ['settings_account'] = "Minhas configurações";
+    $lang ['logout'] = "Logout";
+    $lang ['settings_chat'] = "Configurações das discussões";
+
+    $lang ['signin'] = "Conexão";
+    $lang ['inscrição'] = "Inscrição";
+    $lang ['forgot_password'] = "Senha perdida?";
+    $lang ['about'] = "Sobre Dev-Time";
+    $lang ['terms'] = "Condições de Uso";
+    $lang ['faq'] = "FAQ";
+    $lang ['download'] = "Baixar";
+
+    $lang ['your_email'] = "Seu email";
+    $lang ['your_username'] = "Seu apelido";
+    $lang ['your_password'] = "Sua senha";
+    $lang ['confirm_your_password'] = "Confirme sua senha";
+
+    $lang ['success_signup'] = "Você foi registrado com sucesso!";
+    $lang ['account_exist_signup'] = "Já existe uma conta sob esses identificadores";
+    $lang ['pwd_error_txt'] = "As suas senhas não são identicas";
+    $lang ['error__input_clear'] = "Alguns campos estão vazios";
+    $lang ['login_success'] = "Conexão...";
+    $lang ['error__account_not_found'] = "Nenhuma conta existe em Dev-Time";
+    $lang ['error__account_suspend'] = "Sua conta está suspensa";
+    $lang ['forgot_password_reset'] = "Redefinir";
+
+    $lang ['chat_txt'] = "Discussões em andamento";
+    $lang ['chat_txt2'] = "Discussoes pendentes";
+    $lang ['settings'] = "Configurações";
+    $lang ['new_conversation'] = "Nova conversa";
+    $lang ['add_conversation'] = "Adicionar à conversa";
+    $lang ['blocked'] = "Bloquear";
+
+    $lang ['search'] = "Pesquisar";
+    $lang ['chats'] = "Discussões";
+    $lang ['view_all_chat'] = "Ver todas as mensagens";
+    $lang ['notifications'] = "Notificações";
+    $lang ['mark_all_read'] = "Marcar tudo como lido";
+    $lang ['view_all_notifications'] = "Ver todas as notificações";
+
+    $lang ['online'] = "Online";
+    $lang ['offline'] = "Offline";
+    $lang ['coming_soon'] = "Esta opção está chegando em Dev-Time!";
+
+    // PERFIL
+    $lang ['posts_user'] = "Publicações";
+    $lang ['about_user'] = "Sobre";
+    $lang ['followers_user'] = "Assinaturas";
+    $lang ['projects_user'] = "Projetos";
+    $lang ['courses_user'] = "Tutoriais";
+
+    $lang ['add_project'] = "Adicionar um projeto";
+    $lang ['project_link'] = "Link do projeto";
+    $lang ['button_add_project'] = "Adicionar";
+    $lang ['button_close'] = "Fechar";
+    $lang ['add_project_success'] = "Seu projeto foi adicionado com sucesso!";
+
+    $lang ['publish_profile_to'] = "publicou no perfil de %s";
+    $lang ['share_profile_to'] = "compartilhou a publicação de %s";
+    $lang ['bio'] = "Biografia";
+    $lang ['infos_profile'] = "Informações do perfil";
+    $lang ['repport_profile'] = "Denunciar perfil";
+    $lang ['repport_profile_success'] = "Este perfil foi denunciado com sucesso!";
+    $lang ['repport_profile_error'] = "Não foi possível denunciar este perfil";
+    $lang ['blocked_profile'] = "Bloquear perfil";
+    $lang ['deblocked_profile'] = "Desbloquear perfil";
+    $lang ['blocked_profile_success'] = "Você bloqueou este perfil";
+    $lang ['deblocked_profile_success'] = "Você desbloqueou este perfil";
+
+    $lang ['edit__username_success'] = "Você mudou seu apelido com sucesso!";
+    $lang ['edit__username_error'] = "Oops! Este apelido já existe";
+    $lang ['edit__url_success'] = "Você mudou seu link com sucesso!";
+    $lang ['edit__url_error'] = "Oops! Este link já existe ";
+    $lang ['edit__logo_success'] = "Você mudou o seu logo com sucesso!";
+    $lang ['edit__banner_success'] = "Você mudou o seu banner com sucesso!";
+    $lang ['edit__password_success'] = "Você alterou a sua senha com sucesso!";
+    $lang ['edit__bio_success'] = "Você mudou a sua biografia com sucesso!";
+    $lang ['edit__your_username'] = "Seu nome de usuário";
+    $lang ['edit__your_url'] = "Seu link";
+    $lang ['edit__choose_file'] = "Escolha um arquivo";
+    $lang ['edit__password'] = "Senha";
+    $lang ['edit__bio'] = "Sua biografia";
+    $lang ['edit__button_settings'] = "Alterar minhas configurações";
+    $lang ['your_network'] = "Suas redes sociais";
+    $lang ['your_twitter'] = "Seu Twitter (Ex: @DevTimeEU)";
+    $lang ['your_discord'] = "Seu Discord (exemplo: 368832865850621952)";
+    $lang ['your_twitch'] = "Seu Twitch (Ex: yt_trix_folloow)";
+    $lang ['your_yt'] = "Seu canal do YouTube (Ex: UCqK3KdmruqJqKZ8uB-_x_5Q)";
+    $lang ['update_network'] = "Atualizar minhas redes";
+    $lang ['twitter_success'] = "Seu Twitter foi atualizado";
+    $lang ['twitch_success'] = "Seu Twitch foi atualizado";
+    $lang ['discord_success'] = "Seu Discord foi atualizado";
+    $lang ['yt_success'] = "Seu YouTube foi atualizado";
+
+    $lang ['poll_success'] = "Seu voto foi contado";
+    $lang ['poll_voted'] = "Você já votou!";
+    $lang ['poll_error'] = "Oops! É impossível votar";
+    $lang ['poll_login_required'] = "Você precisa estar logado para votar!";
+
+    // POSTS
+    $lang ['mensagem'] = "Mensagem";
+    $lang ['message_button'] = "Publicar mensagem";
+    $lang ['link'] = "Link";
+    $lang ['link_button'] = "Publicar link";
+    $lang ['poll'] = "Enquete";
+    $lang ['add_button_poll'] = "Adicionar uma resposta";
+    $lang ['poll_input'] = "Resposta";
+    $lang ['remove_button_poll'] = "Excluir resposta";
+    $lang ['poll_button'] = "Publicar enquete";
+    $lang ['poll_title'] = "Título da enquete";
+
+    $lang ['message_success'] = "Sua mensagem foi publicada!";
+    $lang ['message_error'] = "Oops! Esta mensagem já foi enviada!";
+    $lang ['link_success'] = "Seu link foi publicado!";
+    $lang ['link_error'] = "Oops! Este link já foi enviado!";
+    $lang ['poll_success'] = "Sua enquete foi publicada!";
+    $lang ['poll_error'] = "Ops! Esta pesquisa já foi enviada!";
+    $lang ['post_delete'] = "Esta postagem acabou de ser removida!";
+    $lang ['post_delete_error'] = "Esta postagem já foi excluída!";
+
+    // 1.0.1 ADIÇÕES
+    $lang ['developer_account'] = "Espaço desenvolvedor";
+    $lang ['developer_bio'] = "Sua biografia de desenvolvedor";
+    $lang ['developer_link'] = "Link";
+    $lang ['developer_name'] = "Nome do seu link";
+    $lang ['developer_add'] = "Adicione um link";
+    $lang ['developer_remove'] = "Excluir link";
+    $lang ['developer_update'] = "Atualizar";
+    $lang ['developer_update_success'] = "Você atualizou as suas informações!";
+    $lang ['developer_lists'] = "Listas de solicitações";
+    $lang ['developer_list_valid'] = "Validar o pedido";
+    $lang ['developer_list_refuse'] = "Rejeitar a solicitação";
+    $lang ['developer_bio_updated'] = "A sua biografia foi atualizada!";
+    $lang ['developer_links_updated'] = "Seus links estão atualizados!";
+
+    $lang ['developer_discord_message'] = "Todo desenvolvedor deve estar no nosso Discord! <a href='https://discord.gg/MwZWYMY'> Inscreva-se agora!</a>";
+    $lang ['developer_demande_language'] = "Idioma (seleção múltipla)";
+    $lang ['developer_demande_presentation'] = "Apresente-se";
+    $lang ['developer_demande_finish_button'] = "Fazer meu pedido";
+    $lang ['developer_error'] = "Erro: Alguns campos estão vazios!";
+    $lang ['developer_demande_error'] = "Você já fez um pedido!";
+    $lang ['developer_demande_success'] = "Sua solicitação acaba de ser feita! Você receberá uma resposta nos próximos 7 dias!";
+    $lang ['developer_demandes'] = "Seus pedidos";
+    $lang ['developer_demandes_wait'] = "Pendente";
+    $lang ['developer_demandes_deny'] = "Rejeitado";
+    $lang ['developer_demandes_success'] = "Validado";
+    $lang ['developer_languages'] = "Idiomas";
+    $lang ['developer_demande_user'] = "Pedido de";
+    $lang ['developer_button_valide'] = "Validar";
+    $lang ['developer_button_deny'] = "Negar";
+    $lang ['developer_demande_error_admin'] = "Você recusou este pedido!";
+    $lang ['developer_demande_success_admin'] = "Sua validação deste pedido!";
+
+    $lang ['developer_faq_question_title'] = "Qual é o meu pedido?";
+    $lang ['developer_faq_demande_description'] = "Sua solicitação para se tornar um desenvolvedor permitirá que você seja referenciado na nossa aplicação. Além disso, uma vez que o seu pedido \"validado\", você será capaz de modificar sua página.<br>
+    Um pedido pode levar até uma semana para ser tratado. No entanto, você não será referenciado diretamente na aplicação, será necessário que atualizemos a aplicação nos vários sistemas operacionais";
+    $lang ['developer_faq_languages_title'] = "Uma linguagem de programação não está na lista. O que fazer?";
+    $lang ['developer_faq_language_description'] = "Nós usamos principalmente as linguagens mais usados, o que significa que as linguagens pouco usadas não são necessariamente postos. No entanto, se for uma linguagem usada por muitos desenvolvedores, atualizaremos para expandir nossa lista.";
+?>
