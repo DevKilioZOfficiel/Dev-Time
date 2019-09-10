09d9664b406c44e75732701e512905e627e1ec2d
KilioZ <trix-folloow@hotmail.com>
2019-09-10 20:16:48 +0200

    1.0.4.1 - Ajouts de 2 lignes sur les MP

--- language/ca/languages_lang.php
+++ language/ca/languages_lang.php
@@ -4,7 +4,7 @@
     $lang['code_language'] = 'CA';
     $lang['code_language_small_txt'] = 'ca';
     $lang['author_language'] = 'KilioZ';
-    $lang['version_language'] = '1.0.5';
+    $lang['version_language'] = '1.0.5.1';
 
     // MENU
     $lang['navigation'] = "Navigation";
@@ -194,13 +194,17 @@
     $lang['course__create_post'] = "Je viens de publier un nouveau tutoriel! Regardez-le ici %s";
 	
 	// 1.0.3.1 AJOUTS
-	$lang['success_like_post'] = "Vous venez d'aimer cette publication avec succès !";
-	$lang['success_dislike_post'] = "Vous venez de ne plus aimer cette publication avec succès !";
+	$lang['success_like_post'] = "Vous venez d'aimer cette publication!";
+	$lang['success_dislike_post'] = "Vous venez de ne plus aimer cette publication!";
 	
 	// 1.0.4 AJOUTS
 	$lang['add_new_conversation_success'] = "Succès! La conversation avec %s est désormais créé!";
 	$lang['add_new_conversation_error'] = "Oups! La conversation avec %s n'a pas été créé!";
 	$lang['messages'] = "Discussions privées";
 	$lang['start_conversation'] = "%s vient de commencer la conversation!";
-	$lang['start_conversation_notification'] = "%s vient de créer une discussion privé";
-	?>
+	$lang['start_conversation_notification'] = "%s vient de créer une discussion privée";
+	
+	// 1.0.4.1 AJOUTS
+	$lang['new_private_message'] = "%s vient d'envoyer un nouveau message!";
+	$lang['user_mp_typing'] = "%s est en train d'écrire!";
+?>
