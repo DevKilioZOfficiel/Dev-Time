7fb22c8452de450ebc98eb827d6b075d5dd0773b
Reydux <31172722+Reydux@users.noreply.github.com>
2019-09-11 08:10:55 +0200

    Corrections et addition des nouveaux ajouts aux langues portugaises. (#7)
    * Version 1.0.3
    
    * Version 1.0.3

--- language/fr/languages_lang.php
+++ language/fr/languages_lang.php
@@ -4,7 +4,7 @@
     $lang['code_language'] = 'FR';
     $lang['code_language_small_txt'] = 'fr';
     $lang['author_language'] = 'KilioZ';
-    $lang['version_language'] = '1.0.8.1';
+    $lang['version_language'] = '1.0.9.1';
 
     // MENU
     $lang['navigation'] = "Navigation";
@@ -193,18 +193,18 @@
     $lang['course__created'] = "Succès! Votre tutoriel est désormais disponible sur votre profil!";
     $lang['course__create_post'] = "Je viens de publier un nouveau tutoriel! Regardez-le ici %s";
 	
-	// 1.0.3.1 AJOUTS
-	$lang['success_like_post'] = "Vous venez d'aimer cette publication!";
-	$lang['success_dislike_post'] = "Vous venez de ne plus aimer cette publication!";
+    // 1.0.3.1 AJOUTS
+    $lang['success_like_post'] = "Vous venez d'aimer cette publication!";
+    $lang['success_dislike_post'] = "Vous venez de ne plus aimer cette publication!";
 	
-	// 1.0.4 AJOUTS
-	$lang['add_new_conversation_success'] = "Succès! La conversation avec %s est désormais créée!";
-	$lang['add_new_conversation_error'] = "Oups! La conversation avec %s n'a pas été créée!";
-	$lang['messages'] = "Discussions privées";
-	$lang['start_conversation'] = "%s vient de commencer la conversation!";
-	$lang['start_conversation_notification'] = "%s vient de créer une discussion privée";
+    // 1.0.4 AJOUTS
+    $lang['add_new_conversation_success'] = "Succès! La conversation avec %s a été créée!";
+    $lang['add_new_conversation_error'] = "Oups! La conversation avec %s n'a pas été créée.";
+    $lang['messages'] = "Discussions privées";
+    $lang['start_conversation'] = "%s vient de commencer la conversation!";
+    $lang['start_conversation_notification'] = "%s vient de créer une discussion privée.";
 	
-	// 1.0.4.1 AJOUTS
-	$lang['new_private_message'] = "%s vient d'envoyer un nouveau message!";
-	$lang['user_mp_typing'] = "%s est en train d'écrire!";
+    // 1.0.4.1 AJOUTS
+    $lang['new_private_message'] = "%s vient d'envoyer un nouveau message!";
+    $lang['user_mp_typing'] = "%s est en train d'écrire!";
 ?>
